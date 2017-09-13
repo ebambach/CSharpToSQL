6e382e617bd38cad6d03d16ada53c590cbc4eaca
bambache@gmail.com <ebambach@users.noreply.github.com>
2017-09-13 09:22:20 -0400

    initial commit

---
+++ CSharpToSQL/Student.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CSharpToSQL {
+	public class Student {
+		//During the first build of the Student class, all of these properties were set to "private."
+		//This meant that the commented out Gets and Sets methods below were needed.  Now... not so much.
+		public int id { get; set; }
+		public string firstname { get; set; }
+		public string lastname { get; set; }
+		public string address { get; set; }
+		public string city { get; set; }
+		public string state { get; set; }
+		public string zipcode { get; set; }
+		public DateTime birthday { get; set; }
+		public string phone { get; set; }
+		public string email { get; set; }
+		public int majorid { get; set; }
+		public int sat { get; set; }
+		public double gpa { get; set; }
+
+		//public int GetId() {
+		//	return id;
+		//}
+		//public void SetId(int ID) {
+		//	id = ID;
+		//}
+		//public string GetFirstName() {
+		//	return firstname;
+		//}
+		//public void SetFirstName(string FirstName) {
+		//	firstname = FirstName;
+		//}
+		//public string GetLastName() {
+		//	return lastname;
+		//}
+		//public void SetLastName(string LastName) {
+		//	lastname = LastName;
+		//}
+		//public string GetAddress() {
+		//	return address;
+		//}
+		//public void SetAddress(string Address) {
+		//	address = Address;
+		//}
+		//public string GetCity() {
+		//	return city;
+		//}
+		//public void SetCity(string City) {
+		//	city = City;
+		//}
+		//public string GetState() {
+		//	return state;
+		//}
+		//public void SetState(string State) {
+		//	state = State;
+		//}
+		//public DateTime GetBirthday() {
+		//	return birthday;
+		//}
+		//public void SetBirthday(DateTime Birthday) {
+		//	birthday = Birthday;
+		//}
+	}
+}
---
+++ TestCSharpToSQL/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using CSharpToSQL;
+
+namespace TestCSharpToSQL {
+	class Program {
+		static void Main(string[] args) {
+			new Program().Run();
+		}
+
+		void Run() {
+			//This string sets up the Server, Database, and whether this is considered to be
+			//trusted connection or not.
+			string connStr = @"Server=STUDENT05;Database=DotNetDatabase;Trusted_Connection=yes";
+			SqlConnection connection = new SqlConnection(connStr);
+			connection.Open();
+			if (connection.State != System.Data.ConnectionState.Open) {
+				Console.WriteLine("SQL Connection did not open.");
+				return;
+			}
+			//The command that we have used to show everything on the Student table, that 
+			//we will be using here in C#.
+			var sql = "select * from Student";
+			//When the connection is open, we want to know that.
+			Console.WriteLine("SQL connection opened successfully.");
+			
+			//When setting up a command, what is it, and where is it going?
+			//("select * from Student", and @"Server=STUDENT05;Database=DotNetDatabase;Trusted_Connection=yes")
+			SqlCommand cmd = new SqlCommand(sql, connection);
+			SqlDataReader reader = cmd.ExecuteReader();
+
+
+			List<Student> students = new List<Student>();
+			
+			//The condition in the while loop, reader.Read(), returns true, so long as there are
+			//additional rows in the table.
+			//This particular loop reads the Id (primary key), FirstName, LastName, Address, City,
+			//State, and Birthday fields for the row that it is currently looking at.  That information
+			//is then printed to the Console.
+			while (reader.Read()) {
+				//reader.GetOrdinal() allows us to not need to know which column # a column uses, so long
+				//as we know what the column is called.
+				var id = reader.GetInt32(reader.GetOrdinal("Id")); //This uses an int, so we need to GetInt32
+				var firstname = reader.GetString(reader.GetOrdinal("FirstName")); //Uses string, so GetString
+				var lastname = reader.GetString(reader.GetOrdinal("LastName"));
+				var address = reader.GetString(reader.GetOrdinal("Address"));
+				var city = reader.GetString(reader.GetOrdinal("City"));
+				var state = reader.GetString(reader.GetOrdinal("State"));
+				var zipcode = reader.GetString(reader.GetOrdinal("Zipcode"));
+				var phone = reader.GetString(reader.GetOrdinal("PhoneNumber"));
+				var email = reader.GetString(reader.GetOrdinal("Email"));
+				var birthday = reader.GetDateTime(reader.GetOrdinal("Birthday")); //Time to GetDateTime
+				
+				//We need to initiall set majorid to 0 here, because in the SQL table that this code pulls from,
+				//majorid is allowed to be null.  It represents which field of study a student has selected,
+				//so if a major has yet to be declared, this will be null.  Not good to have in C#, so the second
+				//thing that we do is to check if the Value is not null, as designated by the ! at the front
+				//of the condition below.  So long as it does have a value, we can set majorid to the int in
+				//that field.
+				var majorid = 0;
+				if (!reader.GetValue(reader.GetOrdinal("MajorId")).Equals(DBNull.Value)){
+					majorid = reader.GetInt32(reader.GetOrdinal("MajorId"));
+				}
+
+				var sat = reader.GetInt32(reader.GetOrdinal("SAT"));
+				var gpa = reader.GetDouble(reader.GetOrdinal("GPA"));
+				Console.WriteLine("");
+				Console.WriteLine($"{id}:{firstname} {lastname} lives at {address}, {city}, {state} {zipcode}.");
+				Console.WriteLine($"Their birthday is {birthday}.");
+				Console.WriteLine($"{firstname}'s phone # is {phone}, and their email is {email}.");
+				Console.WriteLine($"Their SAT score is {sat}, GPA is {gpa}, and major is {majorid}.");
+
+				Student student = new Student();
+				//During the first build of this Program, the below items were some variation of 
+				//student.Get***, which was the relevant method in the Student class.  Because the
+				//properties of the student class are "public" in this build, we are able to directly
+				//say that student.*** = "whatever we want"
+				student.id = id;
+				student.firstname = firstname;
+				student.lastname = lastname;
+				student.address = address;
+				student.city = city;
+				student.state = state;
+				student.zipcode = zipcode;
+				student.birthday = birthday;
+				student.phone = phone;
+				student.email = email;
+				student.majorid = majorid;
+				student.sat = sat;
+				student.gpa = gpa;
+
+				//Now that the student class has been used to set the data, the student is ready to be
+				//added to the "students" list.
+				students.Add(student);
+			}
+			Console.WriteLine("");
+
+			//Close the connection when finished!
+			reader.Close();
+			connection.Close();
+			Console.WriteLine("SQL connection and reader closed.");
+		}
+	}
+}
